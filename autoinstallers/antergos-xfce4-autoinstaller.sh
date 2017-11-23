dd9a9402e47d0d1b2ec3a0fdb8e04a2ae31d02a0
DonutDeflector <donutdeflector@tuta.io>
2017-11-23 17:57:10 +0000

    added laptop optimizations, added gparted

--- autoinstallers/antergos-xfce4-autoinstaller.sh
+++ autoinstallers/antergos-xfce4-autoinstaller.sh
@@ -16,14 +16,49 @@
 ###########################################################
 
 # Packages Installed from the Official Repos
-sudo pacman -S
-  xfce4-whiskermenu-plugin
-  xfce4-sensors-plugin
-  xfce4-systemload-plugin
-  arc-gtk-theme
+sudo pacman -S \
+  xfce4-whiskermenu-plugin \
+  xfce4-sensors-plugin \
+  xfce4-systemload-plugin \
+  arc-gtk-theme \
+  gparted
 
 # Packages Installed from the AUR
-yaourt -S --noconfirm
-  lmms-beta
-  visual-studio-code
-  inox-bin
+yaourt -S --noconfirm \
+  lmms-beta \
+  visual-studio-code \
+  inox-bin \
+  redshift-gtk
+
+# Inox Browser Extensions
+cd
+git clone https://github.com/DonutDeflector/inoxunpack.git
+cd inoxunpack
+./inoxunpack.py lighthouse
+./inoxunpack.py https-everywhere
+./inoxunpack.py privacy-badger
+./inoxunpack.py res
+./inoxunpack.py ublock-origin
+cd
+
+# Laptop Optimizations
+if [ "$1" = "laptop" ]
+then
+  sudo pacman -S powertop tlp
+  sudo cat >/home/phillip/Downloads/cool.txt <<EOL
+[Unit]
+Description=Powertop tunings
+
+[Service]systemctl enable tlp.service  
+systemctl enable tlp-sleep.service
+Type=oneshot
+ExecStart=/usr/bin/powertop --auto-tune
+
+[Install]
+WantedBy=multi-user.target
+EOL
+  sudo systemctl enable powertop
+  sed -i 's/MAX_LOST_WORK_SECS_ON_BAT=60/MAX_LOST_WORK_SECS_ON_BAT=15/g' /etc/default/tlp
+  systemctl enable tlp.service  
+  systemctl enable tlp-sleep.service
+fi
